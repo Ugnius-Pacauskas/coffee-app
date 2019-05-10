5c537a0ee11904b4a6a46e0b870fbbcacc0fb237
Ugnius-Pacauskas <ugnius.pacauskas@gmail.com>
2019-05-10 13:52:37 +0300

    webpack config "hell", preparing for local storage

--- src/coffe-board/CoffeData.js
+++ src/coffe-board/CoffeData.js
@@ -1,8 +1,8 @@
-import latteImage from "./latte.jpg"
-import cappuccinoImage from "./cappuccino.jpg"
-import expressoImage from "./expresso.jpg"
-import mintMochaImage from "./mint-mocha.jpg"
-import mocha from "./mocha.jpg"
+// import latteImage from "./latte.jpg"
+// import cappuccinoImage from "./cappuccino.jpg"
+// import expressoImage from "./expresso.jpg"
+// import mintMochaImage from "./mint-mocha.jpg"
+// import mocha from "./mocha.jpg"
 
 const coffeData = [
     {
@@ -10,35 +10,40 @@ const coffeData = [
         name: "Latte",
         price: 2.00,
         alt: "Latte",
-        imageUrl: latteImage,
+        // imageUrl: latteImage,
+        imageUrl: ''
     },
     {
         id: 1,
         name: "Expresso",
         price: 2.00,
         alt: "Expresso",
-        imageUrl: cappuccinoImage,
+        // imageUrl: cappuccinoImage,
+        imageUrl: ''
     },
     {
         id: 2,
         name: "Cappucino",
         price: 3,
         alt: "Cappucino",
-        imageUrl: expressoImage,
+        // imageUrl: expressoImage,
+        imageUrl: ''
     },
     {
         id: 3,
         name: "Mocha",
         price: 2.40,
         alt: "Mocha",
-        imageUrl: mocha,
+        // imageUrl: mocha,
+        imageUrl: ''
     },
     {
         id: 4,
         name: "Mint mocha",
         price: 3.00,
         alt: "Mint mocha",
-        imageUrl: mintMochaImage,
+        // imageUrl: mintMochaImage,
+        imageUrl: ''
     },
 ]
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './index.scss';
+import App from './App.js';
+import * as serviceWorker from './serviceWorker.js';
 
-// ReactDOM.render(<h1>Hello world!!!</h1>, document.getElementById('root'));
+ReactDOM.render(<h1>Hello world!!!</h1>, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
